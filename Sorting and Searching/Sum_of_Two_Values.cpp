37f8c37086a69d3560acbfbd6bee8b95bc84b8d9
ParthAdhana <84701021+ParthAdhana@users.noreply.github.com>
2022-07-26 08:30:08 +0530

    Add files via upload

---
+++ Sorting and Searching/Sum_of_Two_Values.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*    *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *    *    *   */
+         
+/*
+ 
+ 
+ 
+*/
+ 
+void solve()
+{
+    ll n,k;cin>>n>>k;
+    vector<ll>v(n);
+    map<ll,ll>mp;
+    for (int i = 0; i < n; ++i)
+    {
+        cin>>v[i];
+        mp[v[i]]=i+1;
+    }
+    
+    debug(mp)
+    for(int i=0;i<n;i++)
+    {
+        if(2*v[i]==k)
+        {
+            if(mp[v[i]]==i+1)continue;
+            else 
+            {
+                cout<<i+1<<' '<<mp[v[i]]<<endl;
+                return;
+            }
+        }
+        else
+        {
+            if(mp[k-v[i]])
+            {
+                debug(v[i])
+                debug(k);
+                cout<<i+1<<' '<<mp[k-v[i]]<<endl;
+                return;
+            }
+        }
+    }
+    cout<<"IMPOSSIBLE"<<endl;
+    
+    
+}
+ 
+int main() {    
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    
+    fastio();
+    ll T;T=1;
+    while(T--){
+        solve();
+    }
+}
