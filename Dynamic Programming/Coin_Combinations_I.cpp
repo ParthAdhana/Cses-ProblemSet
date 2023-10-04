f7f05f5d304cc7b525a8c596b6bdf0784da3437c
Parth Adhana <parth007adhana@gmail.com>
2023-10-04 17:53:55 +0530

    Added Coin Combination 1

--- Dynamic Programming/Coin_Combinations_I.cpp
+++ Dynamic Programming/Coin_Combinations_I.cpp
@@ -1,90 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
- 
 typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
- 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
- 
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
- 
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-/*    *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *    *    *   */
-         
-/*
- 
- 
- 
-*/
- 
-void solve()
+
+ll MOD=1e9+7;
+ll fun(ll sum,vector<ll>&v,vector<ll>&dp)
 {
-    ll n,money;cin>>n>>money;
+	if(sum==0)return 1;
+	if(dp[sum]!=-1)return dp[sum];
+	ll ans=0;
+	for(int i=0;i<v.size();i++)
+	{
+		if(v[i]<=sum)
+		ans+=fun(sum-v[i],v,dp);
+	}
+	return dp[sum]=ans%MOD;
+}
+int main() {    
+	ll n;cin>>n;
+    ll sum;cin>>sum;
+    vector<ll>dp(sum+1,0);
     vector<ll>v(n);
     for (int i = 0; i < n; ++i)
     {
-        cin>>v[i];
+    	cin>>v[i];
     }
-    vector<ll>dp(money+1);
+    
+    // memoization or recursive dp or pull dp
+    //vector<ll>dp(sum+1,-1);
+    //cout<<fun(sum,v,dp)%MOD;
+    
+    // tabulation or iterative dp
     dp[0]=1;
-    for(int i=1;i<=money;i++)
+    for(int sm=1;sm<=sum;sm++)
     {
-        for(int j=0;j<n;j++)
-        {
-            if(i>=v[j])
-            dp[i]+=dp[i-v[j]];
-            dp[i]%=MOD;
-        }
+    	for(int index=0;index<v.size();index++)
+    	{
+    		if(v[index]<=sm)
+    			dp[sm]+=(dp[sm-v[index]])%MOD;
+    		
+    	}
     }
-    cout<<dp[money]<<endl;
-    
-}
- 
-int main() {    
-#ifndef ONLINE_JUDGE
-    freopen("error.txt", "w", stderr);
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-#endif
-    
-    fastio();
-    ll T;T=1;
-    while(T--){
-        solve();
-    }
+    cout<<dp[sum]%MOD<<endl;
 }
